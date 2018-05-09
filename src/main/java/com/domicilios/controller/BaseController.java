d75600fb346cd973d70d15f19c4e60864b538079
Valentina <valentinalenis@hotmail.com>
2018-05-08 21:39:08 -0500

    Dependencia Firebase

---
+++ src/main/java/com/domicilios/controller/BaseController.java
@@ -0,0 +1,28 @@
+package com.domicilios.controller;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BaseController {
+	private static int counter = 0;
+	private static final String VIEW_INDEX = "index";
+	private final static org.slf4j.Logger logger = LoggerFactory.getLogger(BaseController.class);
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String welcome(ModelMap model) {
+		return VIEW_INDEX;
+
+	}
+
+
+	
+	@RequestMapping(value = "/despachador", method = RequestMethod.GET)
+	public String mainPage(ModelMap model) {
+		return "despachador";
+	}
+}
